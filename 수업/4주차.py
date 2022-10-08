dfac3482faa4c32b8f64ec81529973c26d18fead
seiyoon <66265618+seiyoon@users.noreply.github.com>
2022-10-08 15:14:24 +0900

    사다리타기

---
+++ 수업/4주차.py
@@ -0,0 +1,25 @@
+# 사다리타기
+
+t = int(input())
+for _ in range(t):
+    n, m, d = map(int, input().split())
+    ladder = []
+
+    for _ in range(m):
+        ladder.append(list(input()))
+
+    a = 2*d - 2 # *이 있는 x 위치
+    c = m
+
+    while c != 1: # c가 9면 while문 나가기
+        c -= 1
+        tf = True
+        if a - 1 >= 0:
+            if ladder[c][a-1] == '+':
+                a -= 2
+                tf = False
+        if tf and a+1 <= 2*n - 2:
+            if ladder[c][a+1] == '+':
+                a += 2
+
+    print(a//2+1)
